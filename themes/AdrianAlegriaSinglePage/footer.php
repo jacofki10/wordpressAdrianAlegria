49a9c93baad149269ad8f55f388237467d42b8b9
Skylink 00022 <00022skylink@Skylinks-iMac.local>
2019-05-28 13:37:41 +0900

    2 commit

---
+++ themes/AdrianAlegriaSinglePage/footer.php
@@ -0,0 +1,75 @@
+ <!-- Footer -->
+ <footer class="bg-dark text-center p-3 ">
+   <div class="container my-5 text-white ">
+     <h1 class="text-uppercase font-weight-bold px-3 anna">Adrian Alegria.</h1>
+     <div class="copyright text-center my-4">
+       <small>Copyright &copy; 2018<a href="http://diegoarnes.viajaconnos.com/" class="text-white"> Diego Arnes </a></small>
+     </div>
+     <ul class="list-inline mb-0">
+       <li class="list-inline-item">
+         <a href="https://www.facebook.com/adrianalegriaofficial" target="_blank" class="text-light">
+           <span class="fa-stack"><i class="fab fa-facebook-f fa-lg"></i></span>
+         </a>
+       </li>
+       <li class="list-inline-item">
+         <a href="https://www.facebook.com/adrianalegriaofficial" target="_blank" class="text-light">
+           <span class="fa-stack"><i class="fab fa-twitter fa-lg"></i></span>
+         </a>
+       </li>
+       <li class="list-inline-item">
+         <a href="https://www.facebook.com/adrianalegriaofficial" target="_blank" class="text-light">
+           <span class="fa-stack "><i class="fab fa-instagram fa-lg"></i></span>
+         </a>
+       </li>
+       <li class="list-inline-item">
+         <a href="https://www.facebook.com/adrianalegriaofficial" target="_blank" class="text-light">
+           <span class="fa-stack"><i class="fab fa-youtube fa-lg"></i></span>
+         </a>
+       </li>
+       <li class="list-inline-item">
+         <a href="https://www.facebook.com/adrianalegriaofficial" target="_blank" class="text-light">
+           <span class="fa-stack "><i class="fab fa-spotify fa-lg"></i></span>
+         </a>
+       </li>
+     </ul>
+   </div>
+ </footer>
+
+ <!-- Bootstrap core JavaScript -->
+
+ <!-- Plugin JavaScript -->
+ <!--Grid Scroll Effects-->
+ <script src="js/gridLoadingEffects/masonry.pkgd.min.js"></script>
+ <script src="js/gridLoadingEffects/imagesloaded.js"></script>
+ <script src="js/gridLoadingEffects/classie.js"></script>
+ <script src="js/gridLoadingEffects/AnimOnScroll.js"></script>
+ <script>
+   new AnimOnScroll(document.getElementById('grid'), {
+     minDuration: 0.7,
+     maxDuration: 1.7,
+     viewportFactor: 0.4
+   });
+ </script>
+
+ <!-- Contact Form JavaScript -->
+ <script src="js/jqBootstrapValidation.js"></script>
+ <script src="js/contact_me.js"></script>
+
+ <!-- Scroll down JavaScript -->
+ <script>
+   var scrollDown = new SmoothScroll('a[href*="#"]');
+ </script>
+
+ <!-- Custom scripts for this template -->
+ <!-- Optional JavaScript -->
+ <script src="js/main.js"></script>
+ <script src="js/animateMenu/main.js"></script>
+ <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+ <?php wp_footer(); ?>
+ </body>
+
+ </html>
---
+++ themes/AdrianAlegriaSinglePage/header.php
@@ -0,0 +1,235 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <?php wp_head(); ?>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Adrian Alegria</title>
+  <!-- Custom styles Animate menu -->
+  <link rel="stylesheet" type="text/css" href="css/animateMenu.css" />
+  <script src="js/animateMenu/segment.min.js"></script>
+  <script src="js/animateMenu/ease.min.js"></script>
+  <!--Scroll down menu-->
+  <script src="js/smooth-scroll.polyfills.js"></script>
+  <!--Nav Color Change-->
+  <link rel="stylesheet" type="text/css" href="css/navColorChange.css" />
+  <script src="js/navColorChange.js"></script>
+  <!--Grid Scroll Effects-->
+  <link rel="stylesheet" type="text/css" href="css/gridLoadingEffects.css" />
+  <script src="js/gridLoadingEffects/modernizr.custom.js"></script>
+</head>
+
+<body id="home">
+  <!-- Navigation mobile-->
+  <nav class="navbar fixed-top d-lg-none bg-white ">
+    <a class="col-10 navbar-brand no-gutters pl-sm-5 text-center" href="#home">
+      <img src="<?php echo get_theme_file_uri('images/AdrianAlegriaLogoNegro.png') ?> " class="LogoMobile" alt="AdrianAlegria Logo">
+    </a>
+    <div id="menu-icon-wrapper" class="menu-icon-wrapper">
+      <svg width="1000px" height="1000px">
+        <path id="pathA" d="M 300 400 L 700 400 C 900 400 900 750 600 850 A 400 400 0 0 1 200 200 L 800 800"></path>
+        <path id="pathB" d="M 300 500 L 700 500"></path>
+        <path id="pathC" d="M 700 600 L 300 600 C 100 600 100 200 400 150 A 400 380 0 1 1 200 800 L 800 200"></path>
+      </svg>
+      <button class="menu-icon-trigger navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"></button>
+    </div>
+    <div class="collapse navbar-collapse text-center m-3 " id="navbarNav">
+      <ul class="navbar-nav list-inline font-weight-bold">
+        <li class="nav-item m-3">
+          <a data-easing="easeInOutQuad" href="#music" class="menu-icon-trigger navbar-toggler " data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">MUSIC</a>
+        </li>
+        <li class="nav-item m-3">
+          <a data-easing="easeInOutQuad" href="#radio" class="menu-icon-trigger navbar-toggler " data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">RADIO</a>
+        </li>
+        <li class="nav-item m-3">
+          <a data-easing="easeInOutQuad" href="#photos" class="menu-icon-trigger navbar-toggler " data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">PHOTOS</a>
+        </li>
+        <li class="nav-item m-3">
+          <a data-easing="easeInOutQuad" href="#gigs" class="menu-icon-trigger navbar-toggler " data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">GIGS</a>
+        </li>
+        <li class="nav-item m-3">
+          <a data-easing="easeInOutQuad" href="#stores" class="menu-icon-trigger navbar-toggler " data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">STORES</a>
+        </li>
+        <li class="nav-item m-3">
+          <a data-easing="easeInOutQuad" href="#booking" class="menu-icon-trigger navbar-toggler " data-toggle="modal" data-target="#bookingModal" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">BOOKING</a>
+        </li>
+        <li class="nav-item m-3">
+          <a data-easing="easeInOutQuad" href="#about" class="menu-icon-trigger navbar-toggler " data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">ABOUT</a>
+        </li>
+        <div class="row d-flex my-2 ">
+          <li class="list-inline-item mx-0 flex-fill">
+            <a href="https://www.facebook.com/adrianalegriaofficial" target="_blank" class="text-light">
+              <span class="fa-stack fa-1x">
+                <i class="fas fa-circle fa-stack-2x "></i>
+                <i class="fab fa-facebook-f fa-stack-1x text-dark"></i>
+              </span>
+            </a>
+          </li>
+          <li class="list-inline-item mx-0 flex-fill">
+            <a href="https://instagram.com/adrianalegriadj/" target="_blank" class="text-light">
+              <span class="fa-stack fa-1x">
+                <i class="fas fa-circle fa-stack-2x "></i>
+                <i class="fab fa-instagram fa-stack-1x text-dark"></i>
+              </span>
+            </a>
+          </li>
+          <li class="list-inline-item mx-0 flex-fill">
+            <a href="https://soundcloud.com/adrian-alegria" target="_blank" class="text-light">
+              <span class="fa-stack fa-1x">
+                <i class="fas fa-circle fa-stack-2x "></i>
+                <i class="fab fa-soundcloud fa-stack-1x text-dark"></i>
+              </span>
+            </a>
+          </li>
+          <li class="list-inline-item mx-0 flex-fill">
+            <a href="https://www.youtube.com/channel/UCHGwqkwO2lIR7h-oXLVRrUg" target="_blank" class="text-light">
+              <span class="fa-stack fa-1x">
+                <i class="fas fa-circle fa-stack-2x "></i>
+                <i class="fab fa-youtube fa-stack-1x text-dark"></i>
+              </span>
+            </a>
+          </li>
+          <li class="list-inline-item mx-0 flex-fill">
+            <a href="https://twitter.com/AdrianAlegriaDJ" target="_blank" class="text-light">
+              <span class="fa-stack fa-1x">
+                <i class="fas fa-circle fa-stack-2x "></i>
+                <i class="fab fa-twitter fa-stack-1x text-dark"></i>
+              </span>
+            </a>
+          </li>
+        </div>
+      </ul>
+    </div>
+  </nav>
+  <!--desktop-->
+  <div class="img-index container-fluid pl-0 section--1 section">
+    <div class="row m-0 pt-4">
+      <!-- Navigation  desktop-->
+      <nav class="col-3 d-none d-lg-block ">
+        <div><img src="images/AdrianAlegriaLogo.png" alt="AdrianAlegria Logo" class="img-fluid"></div>
+        <div class="menu fixed-top mt-5 ml-3 pt-5 ">
+          <div class="js-replace">
+            <div class="js-replace__item js-replace__item--active">
+              <div class="js-replace__content">
+                <a data-easing="easeInOutQuad" href="#home">
+                  <div class="logo">HOME</div>
+                </a>
+                <a data-easing="easeInOutQuad" href="#music">
+                  <div class="logo">MUSIC</div>
+                </a>
+                <a data-easing="easeInOutQuad" href="#radio">
+                  <div class="logo">RADIO</div>
+                </a>
+                <a data-easing="easeInOutQuad" href="#photos">
+                  <div class="logo">PHOTOS</div>
+                </a>
+                <a data-easing="easeInOutQuad" href="#gigs">
+                  <div class="logo">GIGS</div>
+                </a>
+                <a data-easing="easeInOutQuad" href="#stores">
+                  <div class="logo">STORES</div>
+                </a>
+                <a data-easing="easeInOutQuad" href="#booking">
+                  <div class="logo" data-toggle="modal" data-target="#bookingModal">BOOKING</div>
+                </a>
+                <a data-easing="easeInOutQuad" href="#about">
+                  <div class="logo">ABOUT</div>
+                </a>
+              </div>
+            </div>
+            <div class="js-replace__item">
+              <div class="js-replace__content">
+                <a data-easing="easeInOutQuad" href="#home">
+                  <div class="logo--invert">HOME</div>
+                </a>
+                <a data-easing="easeInOutQuad" href="#music">
+                  <div class="logo--invert">MUSIC</div>
+                </a>
+                <a data-easing="easeInOutQuad" href="#radio">
+                  <div class="logo--invert">RADIO</div>
+                </a>
+                <a data-easing="easeInOutQuad" href="#photos">
+                  <div class="logo--invert">PHOTOS</div>
+                </a>
+                <a data-easing="easeInOutQuad" href="#gigs">
+                  <div class="logo--invert">GIGS</div>
+                </a>
+                <a data-easing="easeInOutQuad" href="#stores">
+                  <div class="logo--invert">STORES</div>
+                </a>
+                <a data-easing="easeInOutQuad" href="#booking">
+                  <div class="logo--invert" data-toggle="modal" data-target="#bookingModal">BOOKING</div>
+                </a>
+                <a data-easing="easeInOutQuad" href="#about">
+                  <div class="logo--invert">ABOUT</div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <!-- Social media-->
+      <div class="col-9 px-0 d-none d-lg-block ">
+        <ul class="list-inline float-right">
+          <li class="list-inline-item mx-0">
+            <a href="https://www.facebook.com/adrianalegriaofficial" target="_blank" class="text-light">
+              <span class="fa-stack fa-1x">
+                <i class="fas fa-circle fa-stack-2x "></i>
+                <i class="fab fa-facebook-f fa-stack-1x text-dark"></i>
+              </span>
+            </a>
+          </li>
+          <li class="list-inline-item mx-0">
+            <a href="https://instagram.com/adrianalegriadj/" target="_blank" class="text-light">
+              <span class="fa-stack fa-1x">
+                <i class="fas fa-circle fa-stack-2x "></i>
+                <i class="fab fa-instagram fa-stack-1x text-dark"></i>
+              </span>
+            </a>
+          </li>
+          <li class="list-inline-item mx-0">
+            <a href="https://soundcloud.com/adrian-alegria" target="_blank" class="text-light">
+              <span class="fa-stack fa-1x">
+                <i class="fas fa-circle fa-stack-2x "></i>
+                <i class="fab fa-soundcloud fa-stack-1x text-dark"></i>
+              </span>
+            </a>
+          </li>
+          <li class="list-inline-item mx-0">
+            <a href="https://www.youtube.com/channel/UCHGwqkwO2lIR7h-oXLVRrUg" target="_blank" class="text-light">
+              <span class="fa-stack fa-1x">
+                <i class="fas fa-circle fa-stack-2x "></i>
+                <i class="fab fa-youtube fa-stack-1x text-dark"></i>
+              </span>
+            </a>
+          </li>
+          <li class="list-inline-item mx-0">
+            <a href="https://twitter.com/AdrianAlegriaDJ" target="_blank" class="text-light">
+              <span class="fa-stack fa-1x">
+                <i class="fas fa-circle fa-stack-2x "></i>
+                <i class="fab fa-twitter fa-stack-1x text-dark"></i>
+              </span>
+            </a>
+          </li>
+          <li class="list-inline-item mx-0">
+            <a href="https://www.mixcloud.com/adrianalegria/" target="_blank" class="text-light">
+              <span class="fa-stack fa-1x">
+                <i class="fas fa-circle fa-stack-2x "></i>
+                <i class="fab fa-mixcloud fa-stack-1x text-dark"></i>
+              </span>
+            </a>
+          </li>
+          <li class="list-inline-item mx-0">
+            <a href="https://open.spotify.com/artist/24w4dGFG8bDi3wG1qbnDM7" target="_blank" class="text-light">
+              <span class="fa-stack fa-1x">
+                <i class="fas fa-circle fa-stack-2x "></i>
+                <i class="fab fa-spotify fa-stack-1x text-dark"></i>
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
